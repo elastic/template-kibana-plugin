48d0a827435daaeca903e2f4e84a1e2347d78884
Joe Fleming <joe.fleming@gmail.com>
2017-07-24 09:10:57 -0700

    make server test pass by default

--- template/server/__tests__/index.js
+++ template/server/__tests__/index.js
@@ -1,7 +1,7 @@
 import expect from 'expect.js';
 
 describe('suite', () => {
-  it('is a test test', () => {
-    expect(true).to.equal(false);
+  it('is a test', () => {
+    expect(true).to.equal(true);
   });
 });
